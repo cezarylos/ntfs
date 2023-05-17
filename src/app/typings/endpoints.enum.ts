5fca69cd9f7fc1811a7ce55e309477e06d418f1a
cezarylos <cezary.los@upflex.com>
2023-05-17 13:20:27 +0200

    disable eslint

--- src/app/typings/endpoints.enum.ts
+++ src/app/typings/endpoints.enum.ts
@@ -1,6 +1,5 @@
 export enum EndpointsEnum {
   CHECK_FOR_TICKETS = 'check-for-tickets',
-  IS_TOKEN_HOLDERS = 'is-token-holders',
   GET_MY_TOKENS = 'get-my-tokens',
   START_LOTTERY = 'start-lottery',
   GET_TOKENS_AMOUNT_LEFT = 'get-tokens-amount-left'
