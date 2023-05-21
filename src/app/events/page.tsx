5006998e93eabdf7ed0e4058f78145b8fc1670a8
cezarylos <cezary.los@upflex.com>
2023-05-22 01:37:38 +0200

    mocny WIP

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -2,11 +2,11 @@ import { StrapiService } from '@/app/services/strapi.service';
 import dynamic from 'next/dynamic';
 import { ReactElement } from 'react';
 
-const Homepage = dynamic(() => import('@/app/components/Homepage/Homepage'), { ssr: false });
+const Events = dynamic(() => import('@/app/components/Events/Events'), { ssr: false });
 
-const populateFields = ['name', 'description', 'startDate'];
+const populateFields = ['name', 'startDate', 'endDate', 'slug'];
 
 export default async function App(): Promise<ReactElement> {
   const events = await StrapiService.getAllEvents(populateFields);
-  return events && <Homepage events={events} />;
+  return events && <Events events={events} />;
 }
