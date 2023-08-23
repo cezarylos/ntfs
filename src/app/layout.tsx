87da38810f41d48eecdb5c367177a31ec10c4145
cezarylos <cezary.los@upflex.com>
2023-08-23 22:09:23 +0200

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { classNames } from '@/app/utils';
+import { Analytics } from '@vercel/analytics/react';
 import dynamic from 'next/dynamic';
 import { Archivo, Mogra } from 'next/font/google';
 import { ReactElement, ReactNode } from 'react';
@@ -24,6 +25,7 @@ export default function RootLayout({ children }: { children: ReactNode }): React
     <html lang="en" className="bg-violet-950 xl:text-[150%] relative h-full overflow-hidden">
       <body className={classNames(mogra.className, 'relative h-screen overflow-hidden top-0')}>
         <AppLayout>{children}</AppLayout>
+        <Analytics />
       </body>
     </html>
   );
