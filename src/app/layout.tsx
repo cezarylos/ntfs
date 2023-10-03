ea5b7fc73d048982f38ad3b1d4a63cccd60e4519
cezarylos <cezary.los@upflex.com>
2023-10-03 17:50:17 +0200

    add body loader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import { classNames } from '@/app/utils';
 import { Analytics } from '@vercel/analytics/react';
 import dynamic from 'next/dynamic';
 import { Archivo, Mogra } from 'next/font/google';
-import { ReactElement, ReactNode } from 'react';
+import React, { ReactElement, ReactNode } from 'react';
 
 import './globals.css';
 
@@ -24,6 +24,9 @@ export default function RootLayout({ children }: { children: ReactNode }): React
   return (
     <html lang="en" className="bg-violet-950 xl:text-[150%] relative h-full overflow-hidden">
       <body className={classNames(mogra.className, 'relative h-screen overflow-hidden top-0')}>
+        <h1 className="animate-pulse text-white text-4xl absolute top-0 bottom-0 left-0 right-0 m-auto w-fit h-fit">
+          Ładowanie...
+        </h1>
         <AppLayout>{children}</AppLayout>
         <Analytics />
       </body>
